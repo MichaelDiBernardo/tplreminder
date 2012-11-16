c63bbed396a3c1ea1e7dfa7f27aeac7be9141bb8
Michael DiBernardo <mikedebo@gmail.com>
2012-11-16 17:12:39 -0500

    Strip all whitespace from card number when it is given.

--- tpl_base/core/forms.py
+++ tpl_base/core/forms.py
@@ -6,3 +6,11 @@ class LibraryAccountForm(ModelForm):
     class Meta:
         model = LibraryAccount
         fields = ('card_number', 'pin', 'email')
+
+    def clean(self):
+        cleaned_data = super(LibraryAccountForm, self).clean()
+
+        stripped_number = cleaned_data.get('card_number', '').replace(" ", "")    
+        cleaned_data['card_number'] = stripped_number
+
+        return cleaned_data
